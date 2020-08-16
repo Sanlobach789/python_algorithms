6b75c3875be02db3480b676afefe2dad300bf45d
Lobach Aleksandr <sanlobach789@gmail.com>
2020-08-16 10:24:27 +0300

    final commit

--- les_2/task_1.py
+++ les_2/task_1.py
@@ -1,6 +1,8 @@
 # 2. Посчитать четные и нечетные цифры введенного натурального числа. Например, если
 # введено число 34560, в нем 3 четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).
-#
+
+#https://drive.google.com/file/d/1o_gXwepesgXJezw79YXkQE-Hoyyjyv1R/view?usp=sharing
+
 x = int(input("Введите натуральное число: "))
 evnumb_count = 0  # четные
 oddnumb_count = 0  # нечетные
